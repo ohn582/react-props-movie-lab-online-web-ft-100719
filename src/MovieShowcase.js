719c29ef22b55bd708b1d8b1e93f3274ebcfbf8d
Neal JIn Oh <no4259@gmail.com>
2020-02-28 19:19:01 +0000

    Done.

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -3,24 +3,25 @@ import MovieCard from './card-components/MovieCard.js'
 import movieData from './data.js'
 
 export default class MovieShowcase extends Component {
-
+  constructor(props){
+    super(props)
+  }
   generateMovieCards = () => {
     
   }
 
   render() {
+    
+    const movieCards = movieData.map(movie => {
+      return <MovieCard title={movie.title} IMDBRating={movie.IMDBRating} genres={movie.genres} poster={movie.poster}/>
+    })
+    
+    
+    
     return (
       <div id="movie-showcase">
         {this.generateMovieCards()}
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
+        {movieCards}
       </div>
     )
   }
